dedb5dc0004243cc799b3cda6803666dc6a73b04
Thomas CARBUCCIA <thomas.carbuccia@proxigital.fr>
2024-06-05 03:28:03 +0200

    Update README

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import {Box} from "@mui/material";
 export default function Footer() {
     return (
       <Box sx={{py: 3}}>
-          Un challenge de <a target="_blank" href="https://www.youtube.com/@BenjaminCode">Benjamin Code</a> - <a target="_blank" href="https://github.com/Hester60/youtube-thumbnail-downloader">Code Source</a>
+          Un challenge de <a target="_blank" href="https://www.youtube.com/watch?v=f7_CHu0ADhM">Benjamin Code</a> - <a target="_blank" href="https://github.com/Hester60/youtube-thumbnail-downloader">Code Source</a>
       </Box>
     );
 }
