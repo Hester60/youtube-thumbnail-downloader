918093a721c13f8e5868b1f98c35f90f6e2f53f0
Thomas CARBUCCIA <thomas.carbuccia@proxigital.fr>
2024-06-05 03:42:17 +0200

    Update footer with LinkedIn link

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,7 +3,8 @@ import {Box} from "@mui/material";
 export default function Footer() {
     return (
       <Box sx={{py: 3}}>
-          Un challenge de <a target="_blank" href="https://www.youtube.com/watch?v=f7_CHu0ADhM">Benjamin Code</a> - <a target="_blank" href="https://github.com/Hester60/youtube-thumbnail-downloader">Code Source</a>
+          Un challenge de <a target="_blank" href="https://www.youtube.com/watch?v=f7_CHu0ADhM">Benjamin Code</a> - <a target="_blank" href="https://github.com/Hester60/youtube-thumbnail-downloader">Code Source</a> par <a
+          href="https://www.linkedin.com/in/thomas-carbuccia-26750b193/" target="_blank">Thomas CARBUCCIA</a>
       </Box>
     );
 }
